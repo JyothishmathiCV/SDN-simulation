6156891724be83ef4432327146e4c78fdd1779b6
mohanpierce99 <paying.rich6990@gmail.com>
2019-04-22 19:01:58 +0530

    newchange

--- script.js
+++ script.js
@@ -3,9 +3,14 @@ const { spawn } = require('child_process');
 
 let ears=spawn('java',['-jar','dsdn.jar']);
 
+var masterBuffer="";
+var regex=/endgame--/g;
 ears.stdout.on('data',(d)=>{
-    console.log(''+d);
-    console.log("_______________");
+   d=''+d;
+   masterBuffer+=d;
+if(regex.test(d)){
+ console.log(masterBuffer);
+}
 });
 
 
